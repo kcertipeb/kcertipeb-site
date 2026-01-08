f62b41d2eae85000fa1ed11bb7c61a66aa69fb2d
kcertipeb <info@kcertipeb.be>
2026-01-08 02:15:57 +0100

    Modify reply_to field in send-contact-email function
    Updated the reply_to field in the email body.

--- supabase/functions/send-contact-email/index.ts
+++ supabase/functions/send-contact-email/index.ts
@@ -50,7 +50,7 @@ Deno.serve(async (req: Request) => {
       body: JSON.stringify({
         from: "K Certipeb <onboarding@resend.dev>",
         to: "info@kcertipeb.be",
-        reply_to: email,
+        
         subject: "Nouvelle demande PEB",
         html: `
           <h2>Nouvelle Demande de Certificat PEB</h2>
